c38228d85a62dcdf34b7aa157df0c16873d43e51
Lukas Normantas <spam@discoverops.com>
2020-03-29 23:37:19 +0100

    🍎 DI configuration for all classes

--- roundup/src/test/java/dev/luke10x/starling/roundup/RoundupApplicationTests.java
+++ roundup/src/test/java/dev/luke10x/starling/roundup/RoundupApplicationTests.java
@@ -1,16 +1,41 @@
 package dev.luke10x.starling.roundup;
 
+import dev.luke10x.starling.roundup.domain.AccountRoundupCollector;
+import dev.luke10x.starling.roundup.domain.DateResolver;
+import dev.luke10x.starling.roundup.domain.RoundupCommand;
+import dev.luke10x.starling.roundup.domain.TransactionFeedCalculator;
+import dev.luke10x.starling.roundup.http.HttpAccountsProvider;
+import dev.luke10x.starling.roundup.http.HttpTransactionFeedProvider;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
 @TestPropertySource(locations="classpath:test.properties")
 @ActiveProfiles("test")
+@SpringBootTest(classes = { RoundupApplication.class })
 class RoundupApplicationTests {
 
-	@Test
+    @Autowired
+    TransactionFeedCalculator transactionFeedCalculator;
+
+    @Autowired
+    HttpAccountsProvider accountsProvider;
+
+    @Autowired
+    HttpTransactionFeedProvider transactionFeedProvider;
+
+    @Autowired
+    DateResolver dateResolver;
+
+    @Autowired
+    AccountRoundupCollector accountRoundupCollector;
+
+    @Autowired
+    RoundupCommand roundupCommand;
+
+    @Test
 	void contextLoads() {
-	}
+    }
 }
