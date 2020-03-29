f3310825a7be5d42536c37eb60be85fc5cdf14f9
Lukas Normantas <spam@discoverops.com>
2020-03-29 22:42:06 +0100

    🍏 Sums up all items in the feed

--- roundup/src/main/java/dev/luke10x/starling/roundup/domain/TransactionFeedCalculator.java
+++ roundup/src/main/java/dev/luke10x/starling/roundup/domain/TransactionFeedCalculator.java
@@ -5,6 +5,12 @@ import dev.luke10x.starling.roundup.domain.feed.TransactionFeed;
 
 public class TransactionFeedCalculator {
     public Money calculate(TransactionFeed feed) {
-        return new Money("GBP", 0);
+        Money feedRoundup = feed.getFeedItems().stream()
+                .map(item -> item.getAmount())
+                .reduce(
+                        new Money("GBP", 0),
+                        (subtotal, item) -> subtotal.add(item.roundup())
+                );
+        return feedRoundup;
     }
 }
