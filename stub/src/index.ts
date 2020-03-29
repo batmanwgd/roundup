b14a7d99def25d9af40d828794529addafe8aa24
Lukas Normantas <spam@discoverops.com>
2020-03-29 18:10:29 +0100

    Stubs can recreate call to accounts endpoint
    curl --get http://stub:3000/api/v2/accounts -H "Accept:application/json" -H'Authorization: Bearer eyJhbGciOiJQUzI1NiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAAAH1Ty5LbIBD8lS2dly3JRs9bbvmBfMDADDZlCVSAnGyl8u9BQrIsZysXl7p7pqcH8O9Me591GYyaIQ32wwdwvTYXAeb2Ie2QvWd-ErGigLoshBDsXCnFOJ6ANS3WrKk48LpUxOsqFtOvMeuKsuFtk_OqeM80hEQ0Lc9nAqS0kwnfbY_kfmiM3kqeoETZMAUSGS94w4RQLZMngvZE57rGPHoHeyOTOnJ1jiNVw1AVOeOqPjMQSPGnblsOsT6f08S1vklJ3qcugUVblXlsyKmKO8SJIHnNCl7X5xIbVUmcF5Z2pPlQUlJ2XaIyAwN1jgDfXoTwOb4IGskErTS5I99rHw7MChBdDNkR6vAASQkB5HWgR-WOfzod6A2mcLVO-3hlTBvUd40T9KlYQA9GrtEkOGTSmuBsnwbNzKpZo7QbIGhrmFVMTQb9Q_KP6RtIo-Xkgx22FWkAvRoPYBACdUg9xboNLmUDuBuFOe3oSJGjGND_T0qzkjb2ICmuGejilrDPjf-Kays5eYVthYECxDTQyQgXdcVL8hE-iTYpgXWJBPYipge4rDslbftkYupv3XYvtFO7bcK7c8IPg97KeIVP5QvB7HyXr-za5azS_RYqpTxQS5UjSXoMB-CPUjoyD_d4DZ5d7J7jwK3RD9zi88yw4MD4eJFfWeziF167mExDfEHzu7AOn9yO7GZzZLf-QPN_hkl_f6VGVCs1CS9dPITHu1pSgFyI5Uyfibki-_MXqWaobEAFAAA.MhRXdPZHZQ3HKj_0EfqUGUuxtvetrFcRgr-CaOqiGTSUuFVBA5prlqLUTdCzO06vBBORjm1kcikw8KkK5rp63lMuywP3gLI-RfbciJSjNNbEun_RcX6kDryNMk75iKUin7SWow8hi73cgRSWXF37YDyMtQYYLyymqjZbQ9siYgA4PZBYOH9qjNBxyyyHzi9wRYZ1tMFZ6uAfj9ThYs2ecQ41kXR1Jw7XEH0Zp3RFxN49i5RRr4sArie1jq-ZKPRe3YRR1DRLYMKovfO2O_pSX7pP8VmK2FuzFx_gIdQgSOkBz9Yg0QGF3fN6mXl2hJz7baKToC0O7NSXaw-ngMCvOby0i2jOiYGBmcQm2Wv5SIWFwnQMrYm3avr8_w0F-89c_sKVjgn5SiQOwdxth5RcO2Nw9XulwrmE2deZWCs_1K1YsMKqM16C33PhVoPwEg1NbdzhI3OysQdZOFm7QjOetFeA0JkjucxPVYxqum-NjQWyz1JI8AvA4mMAzmimZdAHb-TNu1Md3i4SdO6SDavLXwBzmkNWYfItfAwP3ph2mcyuychq_mScfIpq-CAaAwrbEvR43X7qxE7UEQJC0pbwdIse1qubzESfprzRG4vmqDIN3GxEpCigiaBLSCGWXfiH8nGLXOxcSFhF9Z9Pp8Wi5x6EPPjroboiXcYD37nZDww'

---
+++ stub/src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.get('/api/v2/accounts', ( req, res ) => {
+    const accounts = {
+      "accounts": [
+        {
+          "accountUid": "ac82f660-5442-4b78-9038-2b72b1206390",
+          "defaultCategory": "2eb42e49-f275-4019-8707-81a0637e7206",
+          "currency": "GBP",
+          "createdAt": "2020-03-22T16:21:00.156Z"
+        }
+      ]
+    };
+
+    res.json(accounts);
+} );
+
+app.listen( port, () => {
+  console.log( `server started at http://localhost:${ port }` );
+});
