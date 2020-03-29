d4e3c320e810191720f5fec0d782650a9c0e07c8
Lukas Normantas <spam@discoverops.com>
2020-03-29 19:48:37 +0100

    🍎 First week of 2020 starts on 2019-12-30 Monday

---
+++ roundup/src/test/java/dev/luke10x/starling/roundup/DateResolverTest.java
@@ -0,0 +1,45 @@
+package dev.luke10x.starling.roundup;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DateResolverTest {
+
+    /*
+     *    December 2019
+     * Su Mo Tu We Th Fr Sa
+     *  1  2  3  4  5  6  7
+     *  8  9 10 11 12 13 14
+     * 15 16 17 18 19 20 21
+     * 22 23 24 25 26 27 28
+     * 29 30 31
+     *
+     *     January 2020
+     * Su Mo Tu We Th Fr Sa
+     *           1  2  3  4
+     *  5  6  7  8  9 10 11
+     * 12 13 14 15 16 17 18
+     * 19 20 21 22 23 24 25
+     * 26 27 28 29 30 31
+     */
+
+    @Test
+    @Disabled
+    void firstWeekOf2020StartsOnPreviousYear30OfDecember() {
+        DateResolver dateResolver = new DateResolver();
+
+        LocalDate date = dateResolver.resolve(2020, 1);
+
+        assertEquals(2019, date.getYear());
+        assertEquals(Month.DECEMBER, date.getMonth());
+        assertEquals(30, date.getDayOfMonth());
+        assertEquals(DayOfWeek.MONDAY, date.getDayOfWeek());
+    }
+}
